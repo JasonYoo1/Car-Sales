1297b8e13017b6621976f8bd3934ec647490ef6f
JasonYoo1 <jyoojs@gmail.com>
2019-08-14 21:07:59 -0700

    created reducer for updatefeature

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect  } from 'react-redux'
+import { dispatch } from 'rxjs/internal/observable/pairs';
 
 const AdditionalFeature = props => {
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"
+      onClick={()=> dispatch({ type: 'UPDATE_FEATURE' })
+      }
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+
+const mapStateToProps = state =>{
+  console.log(state)
+  return{
+
+  }
+}
+
+export default connect(mapStateToProps, {})(AdditionalFeature);
